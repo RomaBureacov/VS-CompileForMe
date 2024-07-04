fdb256a540a52f79d962d2a98a37672190afa5e1
RomaBureacov <RomanBureacov@gmail.com>
2024-07-04 16:54:01 -0700

    Appended Necessary Files
    Initial implementation

---
+++ CompileForMe.bat
@@ -0,0 +1,123 @@
+@echo off
+
+:: Set up the Visual Studio environment
+:: CHANGE TO YOUR VISUAL STUDIO ENVIRONMENT SETUP
+:: YOU WILL NEED TO FIND vcvarsall.bat UNDER APPROXIMATELY THE SAME FILE PATH AS IN THE LINE BELOW
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x64
+if errorlevel 1 (
+    echo Failed to load environment, exiting program...
+    pause
+    exit /b 1
+) else (
+    echo Successfully loaded environment
+)
+
+echo.
+
+:: Prompt for the source directory
+:askSourceDirAgain
+set /p "source_dir=Enter source directory: "
+
+:: Remove quotes if implemented
+set "source_dir=%source_dir:"=%"
+
+if not exist "%source_dir%" (
+    echo ^|Directory "%source_dir%" not found, please try again.
+    goto askSourceDirAgain
+)
+echo ^|Source directory found: "%source_dir%"
+echo.
+
+:askSourceFileAgain
+set /p "source_file=Enter cpp file name: "
+
+:: determine if file name with or without .cpp  provided
+set source_file_name=%source_file%
+echo %source_file%| findstr /i "\.cpp$" >nul
+if errorlevel 1 ( 
+    REM if the file does not include .cpp extension
+    set "source_file=%source_file%.cpp"
+) else (
+    for %%F in ("%source_file%") do set "source_file_name=%%~nF"
+)
+
+:: verify file exists
+if not exist "%source_dir%\%source_file%" (
+    echo ^|File "%source_dir%\%source_file%" not found in directory, please try again.
+    goto askSourceFileAgain
+)
+echo ^|File found: "%source_dir%\%source_file%"
+
+:: compile the program
+:recompileProgram
+set "output_dir=%source_dir%\Compiled"
+if not exist "%output_dir%" (
+    echo ^|Creating: "%output_dir%"
+    mkdir "%output_dir%"
+)
+echo ^|attempting to compile into: "%output_dir%\%source_file_name%.exe"
+echo.
+
+echo ==================== COMPILER ====================
+cl /EHsc "%source_dir%\%source_file%" /Fo"%output_dir%\\" /Fe"%output_dir%\%source_file_name%.exe"
+echo ==================== END COMPILER ====================
+echo.
+
+:: should the program fail to compile
+if errorlevel 1 (
+:askToRecompileAgain
+    echo Program has fail to compile or an error occurred
+    echo 1. Recompile program
+    echo 2. Choose another program to compile
+    set /p choice="Choice: "
+    if "%choice%"=="1" (
+        goto recompileProgram
+    ) else if "%choice%"=="2" (
+        goto askSourceFileAgain
+    ) else (
+        echo ^|Invalid response, please try again.
+        echo.
+        goto askToRecompileAgain
+    )
+) else (
+    echo Program successfully compiled
+)
+
+:: ask to run the compiled program
+:askToRunAgain
+set /p runProgram="Run the compiled program or recompile? (y/n/r): "
+if /i "%runProgram%"=="y" (
+    echo ^|Attempting to run: "%output_dir%\%source_file_name%.exe"
+    start "%source_file_name%" /d "%output_dir%" cmd /k "%source_file_name%.exe"
+
+) else if /i "%runProgram%"=="r" (
+    goto recompileProgram
+) else if /i not "%runProgram%"=="n" (
+    echo ^|Invalid Response, please try again.
+    goto askToRunAgain
+)
+echo.
+
+:: ask the user if they want to recompile, change directory, or exit
+:chooseNextStep
+echo Choose what to do next
+echo 1. Recompile
+echo 2. Choose another program to compile
+echo 3. Choose another directory
+echo 4. Exit
+set /p choice="Choice: "
+echo.
+
+if "%choice%"=="1" (
+    goto recompileProgram
+) else if "%choice%"=="2" (
+    goto askSourceFileAgain
+) else if "%choice%"=="3" (
+    goto askSourceDirAgain
+) else if "%choice%"=="4" (
+    exit /b 0
+) else (
+    echo ^|Invalid response, please try again.
+    echo.
+    goto chooseNextStep
+)
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # VS CompileForMe
- A batch script for compiling C++ program using Visual Studio's C++ compiler
+ A batch script for compiling C++ program using Visual Studio's C++ compiler. Created because by default Visual Studio has no way to compile one-off scripts for you without opening its terminal and using `cl /EHsc` for everything, and so that initially became really tedious when attempting to learn C++ through all the little scripts the textbook would provide.
+
+## Setup
+
+Download the file `CompileForMe.bat` provided in this repository.
+
+Before you start running the program, you must first edit a small portion of the program. You will need to find the location of the Visual Studio C++ environment so that you can compile scripts into executables. The file you are looking for is called `vsvarsall.bat`, and it will be located in approximately the same directory as 
+
+    C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall.bat
+
+Once you find the directory and the file, simply copy it into your clipboard, the full link you are copying should look similar to the one above, and should include the `vsvarsall.bat`.
+
+Next you will need to open the `.bat` file in a text editor. Simply right-click and choose *Edit* or open with your preferred text editor.
+
+You will navigate to one of the very first lines of code that starts with `call`. From here, you will paste your file directory over the one in the script, such that it will call the Visual Studio environment correctly when it comes time to compile.
+
+You have now successfully set up the program and are free to compile your short, one-off scripts!
+
+## Use
+
+Simply follow the instructions the program provides. Note that when it asks for the directory, it is sensitive to spaces, so if your directory includes spaces, *you must enclose the directory in quotes*.
+
+For example, an invalid directory name would be:
+
+    Z:\Documents\My C++\Programs and stuff
+
+Instead you must enclose it in quotes when providing it to the batch script as such:
+
+    "Z:\Documents\My C++\Programs and stuff"
+
+If the VS compiler fails to compile your C++ program, the script will tell you that it fail to compile and wait for your response on what to do next (either if you wish to recompile or proceed to attempt to compiler another script).
+
+Once your program compiles successfully, you can choose to run it, move on, or recompile.
+
+At the end, you choose either to recompile the same script, use another file, use another directory, or exit the program.
+
+Have fun programming!
